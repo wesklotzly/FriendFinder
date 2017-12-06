568b2e08c75316e804f72c470e5f3c02333c1d4c
wesklotzly <stuwn420@gmail.com>
2017-12-05 22:28:58 -0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,22 @@
+// dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// Set up Express
+var app = express();
+var PORT = process.env.PORT || 3000; //var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+require('./app/routing/apiRoutes')(app);
+require('./app/routing/htmlRoutes')(app);
+
+// Starts server
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
